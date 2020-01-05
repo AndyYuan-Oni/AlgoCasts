e4f5fa357af82a41a79fb00b4623036d7726655e
Andy <andyyuan2017@gmail.com>
2020-01-06 10:31:10 +1100

    reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,20 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    return str.split('').reduce((rev, char) => char + rev, '');
+
+}
+//solution by me
+// function reverse(str) {
+//     var n = str.length;
+//     var reversed = "";
+
+//     for (var i = n - 1; i >= 0; i--) {
+//         reversed = reversed + str[i];
+//     }
+//     return reversed;
+
+// }
 
 module.exports = reverse;
